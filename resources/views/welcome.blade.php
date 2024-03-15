4215037276d984e9ede342c88ed967af3beff143
TheGrayish <yezael01@hotmail.com>
2024-03-15 12:08:07 -0600

    Welcome page modificada, para que redireccione a la main page (poniendo una pequeña descripcion del proyecto)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,8 +38,25 @@
                     </svg>
                 </div>
 
-                <div class="mt-16">
-                    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+                <div class="mt-16">   
+
+
+                    <a href="/main" class="scale-100 p-6 bg-white from-gray-700/50 via-transparent rounded-lg shadow-2xl shadow-gray-500/20 flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                        <div>
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900">QCI-Hub</h2>
+                            <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+                                Una plataforma que permita a estudiantes resolver, repasar y analizar los temas, dándole la oportunidad de poder aprender y
+                                así les sea más fácil el aprobar la materia para evitar que se vean en la necesidad de recursar la materia por la falta de 
+                                conocimiento o información que sirva relacionada al tema de interés.
+                            </p>
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                        </svg>
+                    </a>
+                    
+
+                 {{-- <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                         <a href="https://laravel.com/docs" class="scale-100 p-6 bg-white from-gray-700/50 via-transparent rounded-lg shadow-2xl shadow-gray-500/20 flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                             <div>
                                 <div class="h-16 w-16 bg-red-50 flex items-center justify-center rounded-full">
@@ -115,7 +132,7 @@
                                 </p>
                             </div>
                         </div>
-                    </div>
+                    </div>  --}}
                 </div>
 
                 <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">

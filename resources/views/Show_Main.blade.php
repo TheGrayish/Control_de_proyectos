75d0cbcf13c7b9aa28d6991f8cd11bff11dc553f
Kevin Ramses <kevinramsesgamer@gmail.com>
2024-05-08 21:09:34 -0600

    Primer Intento de front Show

--- resources/views/Show_Main.blade.php
+++ resources/views/Show_Main.blade.php
@@ -5,6 +5,88 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Detalles del Archivo</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #000000;
+            background-image: linear-gradient(135deg, #000000 0%, #333333 100%);
+            background-size: cover;
+            background-attachment: fixed;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 50px auto;
+            background-color: #222222;
+            padding: 30px;
+            border-radius: 10px;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+        }
+
+        h2 {
+            font-size: 32px;
+            margin-bottom: 30px;
+            color: #ffffff;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+        }
+
+        .card {
+            background-color: #333333;
+            border-radius: 10px;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+            padding: 20px;
+            margin-bottom: 20px;
+        }
+
+        .card-body {
+            color: #ffffff;
+        }
+
+        .card-text {
+            margin-bottom: 10px;
+        }
+
+        .btn {
+            padding: 10px 20px;
+            border-radius: 6px;
+            cursor: pointer;
+            text-transform: uppercase;
+            font-weight: bold;
+            transition: background-color 0.3s ease;
+        }
+
+        .btn-primary {
+            background-color: #555555;
+            color: #fff;
+            border: none;
+        }
+
+        .btn-primary:hover {
+            background-color: #444444;
+        }
+
+        .btn-warning {
+            background-color: #777777;
+            color: #fff;
+            border: none;
+        }
+
+        .btn-warning:hover {
+            background-color: #666666;
+        }
+
+        .btn-danger {
+            background-color: #990000;
+            color: #fff;
+            border: none;
+        }
+
+        .btn-danger:hover {
+            background-color: #770000;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
